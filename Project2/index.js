39c1b9e9e1483dcda2c7878c10ff6ca7661c1b30
Utsav <pk0385599@gmail.com>
2024-09-14 01:58:25 +0530

    Completd the lowest level project where i learnt about backend basic stuff

--- Project2/index.js
+++ Project2/index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const path=require('path');
 const app=express();
+const fs=require('fs');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -9,7 +10,21 @@ app.use(express.static(path.join(__dirname,"public")));
 
 
 app.get("/",(req,res)=>{
-    res.render("index");
+    fs.readdir(`./files`,(err,files)=>{
+        res.render("index",{files:files});
+        // console.log(files);
+    })
+});
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render('show',{filename:req.params.filename,filedata:filedata});
+    })
+});
+app.post("/create",(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=> {
+        console.log(err);
+        res.redirect('/');
+    });
 });
 
 app.listen(3000);
